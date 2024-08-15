aeef1829666765017e2d82147b8edecdd11ccda5
alin-luca <alinluca20021@gmail.com>
2024-08-15 18:02:18 +0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config;
